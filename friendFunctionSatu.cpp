5a973c5c0cfe1f0a625f91d3c6e1ad7c8ce4faf9
pasyaa <mhmdpsyh@gmail.com>
2026-04-28 15:38:02 +0700

    a.nama = b;

---
+++ friendFunctionSatu.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class mahasiswa{
+    private:
+    string nama;
+
+    public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b){
+    a.nama = b;
+    
+}
